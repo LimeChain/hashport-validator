0ad7a299b244f465c371d33a1a4149624139301e
Georgi Yazovaliyski <georgi.yazovaliiski@gmail.com>
2020-11-11 10:27:59 +0200

    Structure Refactoring
    Signed-off-by: Georgi Yazovaliyski <georgi.yazovaliiski@gmail.com>

--- app/clients/hedera/hedera.go
+++ app/clients/hedera/hedera.go
@@ -1,3 +1,34 @@
 package hedera
 
+import (
+	"Event-Listener/config"
+	hederasdk "github.com/hashgraph/hedera-sdk-go"
+)
+
 // TODO hedera client for interacting with hedera nodes
+
+func NewClient() *hederasdk.Client {
+	var client *hederasdk.Client
+	switch config.NetworkType {
+	case "testnet":
+		client = hederasdk.ClientForTestnet()
+	case "mainnet":
+		client = hederasdk.ClientForMainnet()
+	default:
+		panic("Cannot instantiate client. No [config.NetworkType] provided!")
+	}
+
+	accountId, _ := hederasdk.AccountIDFromString(config.AccountData.Operator.AccountId)
+	privateKey, _ := hederasdk.Ed25519PrivateKeyFromString(config.AccountData.Operator.PrivateKey)
+
+	client.SetOperator(accountId, privateKey)
+	return client
+}
+
+func MainAccount() (hederasdk.AccountID, error) {
+	return hederasdk.AccountIDFromString(config.AccountData.Operator.AccountId)
+}
+
+func NewMirrorClient() (hederasdk.MirrorClient, error) {
+	return hederasdk.NewMirrorClient(config.MirrorNodeClientAddress)
+}
--- app/process/watcher/consensus-message/consensus-message.go
+++ app/process/watcher/consensus-message/consensus-message.go
@@ -1,3 +1,42 @@
 package consensus_message
 
-// todo consensus message watcher
+import (
+	"Event-Listener/config"
+	hederasdk "github.com/hashgraph/hedera-sdk-go"
+	"log"
+)
+
+type ConsensusTopicWatcher struct {
+	TopicID hederasdk.ConsensusTopicID
+}
+
+func (ctw ConsensusTopicWatcher) Watch( /* TODO: add SDK queue as a parameter */ ) {
+	subscribeToTopic(ctw.TopicID /* TODO: add SDK queue as a parameter */)
+}
+
+func subscribeToTopic(topicId hederasdk.ConsensusTopicID /* TODO: add SDK queue as a parameter */) {
+	client, e := hederasdk.NewMirrorClient(config.MirrorNodeAPIAddress)
+	if e != nil {
+		log.Printf("Did not subscribe to [%s].", topicId)
+		return
+	}
+
+	_, e = hederasdk.NewMirrorConsensusTopicQuery().
+		SetTopicID(topicId).
+		Subscribe(
+			client,
+			func(response hederasdk.MirrorConsensusTopicResponse) {
+				log.Printf("[%s] - Topic [%s] - Response incoming: [%s]", response.ConsensusTimestamp, topicId, response.Message)
+				// TODO: Push response to SDK queue
+			},
+			func(err error) {
+				log.Printf("Error incoming: [%s]", err)
+			},
+		)
+
+	if e != nil {
+		log.Printf("Did not subscribe to [%s].", topicId)
+		return
+	}
+	log.Printf("Subscribed to [%s] successfully.", topicId)
+}
