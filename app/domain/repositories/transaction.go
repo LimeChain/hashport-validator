13054ccfcc6d3aa08ae899968b76971abb30d624
failfmi <oscurocalma@gmail.com>
2020-11-17 13:22:05 +0200

    feat: transaction repository (#25)
    Signed-off-by: failfmi <oscurocalma@gmail.com>

--- app/domain/repositories/transaction.go
+++ app/domain/repositories/transaction.go
@@ -1,3 +1,11 @@
 package repositories
 
-// TODO interface for the transaction repository
+import "github.com/limechain/hedera-eth-bridge-validator/proto"
+
+type TransactionRepository interface {
+	Exists(transactionId string) error
+	Create(ct *proto.CryptoTransferMessage) error
+	UpdateStatusCancelled(txId string) error
+	UpdateStatusCompleted(txId string) error
+	UpdateStatusSubmitted(txId string, submissionTxId string) error
+}
