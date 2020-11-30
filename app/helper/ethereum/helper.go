4f316b10130449485075e689099179fe98d661df
failfmi <oscurocalma@gmail.com>
2020-11-30 20:41:51 +0200

    fix: handle {27,28} and {0,1} scenarios
    Signed-off-by: failfmi <oscurocalma@gmail.com>

--- app/helper/ethereum/helper.go
+++ app/helper/ethereum/helper.go
@@ -2,6 +2,7 @@ package ethereum
 
 import (
 	"encoding/hex"
+	"errors"
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/limechain/hedera-eth-bridge-validator/app/helper"
@@ -62,14 +63,25 @@ func EncodeData(ctm *proto.CryptoTransferMessage) ([]byte, error) {
 		feeBn)
 }
 
-func DecodeSignature(signature string) ([]byte, error) {
+func DecodeSignature(signature string) (decodedSignature []byte, ethSignature []byte, err error) {
 	decodedSig, err := hex.DecodeString(signature)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
+	}
+
+	if len(decodedSig) != 65 {
+		return nil, nil, errors.New("invalid signature length")
 	}
 
 	// note: https://github.com/ethereum/go-ethereum/issues/19751
-	decodedSig[64] -= 27
+	ethSig := decodedSig
+	if decodedSig[64] == 0 || decodedSig[64] == 1 {
+		ethSig[64] += 27
+	}
 
-	return decodedSig, nil
+	if decodedSig[64] == 27 || decodedSig[64] == 28 {
+		decodedSig[64] -= 27
+	}
+
+	return decodedSig, ethSig, nil
 }
--- app/process/handler/consensus-message/consensus-message.go
+++ app/process/handler/consensus-message/consensus-message.go
@@ -56,7 +56,7 @@ func (cmh ConsensusMessageHandler) handlePayload(payload []byte) error {
 
 	log.Printf("New Consensus Message for processing Transaction ID [%s] was received\n", m.TransactionId)
 
-	decodedSig, err := ethhelper.DecodeSignature(m.GetSignature())
+	decodedSig, ethSig, err := ethhelper.DecodeSignature(m.GetSignature())
 	if err != nil {
 		return errors.New(fmt.Sprintf("[%s] - Failed to decode signature. - [%s]", m.TransactionId, err))
 	}
@@ -85,13 +85,13 @@ func (cmh ConsensusMessageHandler) handlePayload(payload []byte) error {
 		return errors.New(fmt.Sprintf("[%s] - Address is not valid - [%s]", m.TransactionId, address.String()))
 	}
 
-	messages, err := cmh.repository.GetTransaction(m.TransactionId, m.Signature)
+	messages, err := cmh.repository.GetTransaction(m.TransactionId, hex.EncodeToString(ethSig))
 	if err != nil {
-		return errors.New(fmt.Sprintf("Failed to retrieve messages for TxId [%s], with signature [%s]. - [%s]", m.TransactionId, m.Signature, err))
+		return errors.New(fmt.Sprintf("Failed to retrieve messages for TxId [%s], with signature [%s]. - [%s]", m.TransactionId, hex.EncodeToString(ethSig), err))
 	}
 
 	if len(messages) > 0 {
-		return errors.New(fmt.Sprintf("Duplicated Transaction Id and Signature - [%s]-[%s]", m.TransactionId, m.Signature))
+		return errors.New(fmt.Sprintf("Duplicated Transaction Id and Signature - [%s]-[%s]", m.TransactionId, hex.EncodeToString(ethSig)))
 	}
 
 	err = cmh.repository.Create(&message.TransactionMessage{
@@ -99,11 +99,11 @@ func (cmh ConsensusMessageHandler) handlePayload(payload []byte) error {
 		EthAddress:    m.EthAddress,
 		Amount:        m.Amount,
 		Fee:           m.Fee,
-		Signature:     m.Signature,
+		Signature:     hex.EncodeToString(ethSig),
 		Hash:          hexHash,
 	})
 	if err != nil {
-		return errors.New(fmt.Sprintf("Could not add Transaction Message with Transaction Id and Signature - [%s]-[%s]", m.TransactionId, m.Signature))
+		return errors.New(fmt.Sprintf("Could not add Transaction Message with Transaction Id and Signature - [%s]-[%s]", m.TransactionId, hex.EncodeToString(ethSig)))
 	}
 
 	log.Printf("Successfully verified and persisted TX with ID [%s]\n", m.TransactionId)
