e1a4c6a9dbba6868731257e546efa987b0ab799e
failfmi <oscurocalma@gmail.com>
2020-12-01 08:18:52 +0200

    fix(eth-signer): signature decoding (#40)
    * handle 0/1 and 27/28 signatures
    Signed-off-by: failfmi <oscurocalma@gmail.com>

--- app/helper/ethereum/helper.go
+++ app/helper/ethereum/helper.go
@@ -1,6 +1,8 @@
 package ethereum
 
 import (
+	"encoding/hex"
+	"errors"
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/limechain/hedera-eth-bridge-validator/app/helper"
@@ -59,4 +61,25 @@ func EncodeData(ctm *proto.CryptoTransferMessage) ([]byte, error) {
 		common.HexToAddress(ctm.EthAddress),
 		amountBn,
 		feeBn)
+}
+
+func DecodeSignature(signature string) (decodedSignature []byte, ethSignature string, err error) {
+	decodedSig, err := hex.DecodeString(signature)
+	if err != nil {
+		return nil, "", err
+	}
+
+	if len(decodedSig) != 65 {
+		return nil, "", errors.New("invalid signature length")
+	}
+
+	// note: https://github.com/ethereum/go-ethereum/issues/19751
+	ethSig := decodedSig
+	if decodedSig[64] == 0 || decodedSig[64] == 1 {
+		ethSig[64] += 27
+	} else if decodedSig[64] == 27 || decodedSig[64] == 28 {
+		decodedSig[64] -= 27
+	}
+
+	return decodedSig, hex.EncodeToString(ethSig), nil
 }
