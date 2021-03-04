95ec0cb4fe5cb8701188aed46c055fe1f5a36b61
Ventsi Tsochev <ventsi@limechain.tech>
2021-03-04 10:54:40 +0200

    Service fee and members list unit tests (#113)
    * Service fee and members list unit tests
    
    Signed-off-by: Ventsislav Tsochev <ventsi@limechain.tech>
    
    * naming conventions
    
    Signed-off-by: Ventsislav Tsochev <ventsi@limechain.tech>
    
    * change testify module
    
    Signed-off-by: Ventsislav Tsochev <ventsi@limechain.tech>

---
+++ app/process/model/bridge/members/members_test.go
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2021 LimeChain Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package members_test
+
+import (
+	"github.com/limechain/hedera-eth-bridge-validator/app/process/model/bridge/members"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestSet(t *testing.T) {
+	membersService := members.Members{}
+	newMembers := []string{"0x1aSd", "0x2dSa", "0x3qWe", "0x4eWq"}
+	membersService.Set(newMembers)
+	membersList := membersService.Get()
+	assert.Equal(t, len(newMembers), len(membersList), "Different array length")
+	for i, v := range membersList {
+		assert.Equal(t, newMembers[i], v, "Members not set correctly")
+	}
+}
