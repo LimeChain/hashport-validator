d02391776de73656462e799b5d6ed314800217aa
Ventsi Tsochev <ventsi@limechain.tech>
2021-03-04 11:05:37 +0200

    Deployment script (#109)
    * ehtereum sign message added
    
    Signed-off-by: Ventsislav Tsochev <ventsi@limechain.tech>
    
    * test update
    
    Signed-off-by: Ventsislav Tsochev <ventsi@limechain.tech>
    
    * Automatically update contract-level configurations
    
    Signed-off-by: Ventsislav Tsochev <ventsi@limechain.tech>
    
    * tests fixes
    
    Signed-off-by: Ventsislav Tsochev <ventsi@limechain.tech>
    
    * bridge service error handler update
    
    Signed-off-by: Ventsislav Tsochev <ventsi@limechain.tech>
    
    * remove unused lines of code
    
    Signed-off-by: Ventsislav Tsochev <ventsi@limechain.tech>
    
    * added context for logger, new logs added, UpdateServiceFee and UpdateMembers -> private funcs
    
    Signed-off-by: Ventsislav Tsochev <ventsi@limechain.tech>
    
    * string formating
    
    Signed-off-by: Ventsislav Tsochev <ventsi@limechain.tech>
    
    * rename custodians to members
    
    Signed-off-by: Daniel Ivanov <daniel.k.ivanov95@gmail.com>
    
    * removed unused big int variable
    
    Signed-off-by: Ventsislav Tsochev <ventsi@limechain.tech>
    
    * config setup script
    
    Signed-off-by: Ventsislav Tsochev <ventsi@limechain.tech>
    
    * get account id and private key from command line
    
    Signed-off-by: Ventsislav Tsochev <ventsi@limechain.tech>
    
    * namings
    
    Signed-off-by: Ventsislav Tsochev <ventsi@limechain.tech>
    
    * readme updated
    
    Signed-off-by: Ventsislav Tsochev <ventsi@limechain.tech>
    
    * readme updated, input validations, scheduledTxPayerAccount added
    
    Signed-off-by: Ventsislav Tsochev <ventsi@limechain.tech>
    
    * acc and pr key error handling
    
    Signed-off-by: Ventsislav Tsochev <ventsi@limechain.tech>
    
    * naming: prKey -> privateKey
    
    Signed-off-by: Ventsislav Tsochev <ventsi@limechain.tech>
    
    * Ability to choose network through Command-line argument, Logging the new created account balance
    
    Signed-off-by: Ventsislav Tsochev <ventsi@limechain.tech>
    
    * readme updated
    
    Signed-off-by: Ventsislav Tsochev <ventsi@limechain.tech>
    
    * initClient naming, removed balance querying
    
    Signed-off-by: Ventsislav Tsochev <ventsi@limechain.tech>
    
    * HBar balance as variable
    
    Signed-off-by: Ventsislav Tsochev <ventsi@limechain.tech>
    
    * typo
    
    Signed-off-by: Ventsislav Tsochev <ventsi@limechain.tech>
    
    Co-authored-by: Daniel Ivanov <daniel.k.ivanov95@gmail.com>

---
+++ examples/three-validators/hedera-setup.go
@@ -0,0 +1,145 @@
+/*
+* Copyright 2021 LimeChain Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+ */
+
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/hashgraph/hedera-sdk-go"
+)
+
+var balance = hedera.NewHbar(100)
+
+func main() {
+	privateKey := flag.String("privateKey", "0x0", "Hedera Private Key")
+	accountID := flag.String("accountId", "0.0", "Hedera Account ID")
+	network := flag.String("network", "", "Hedera Network Type")
+	flag.Parse()
+
+	if *privateKey == "0x0" {
+		panic("Private key was not provided")
+	}
+	if *accountID == "0.0" {
+		panic("Account id was not provided")
+	}
+	fmt.Println("-----------Start-----------")
+	client := initClient(*privateKey, *accountID, *network)
+	privKey1, err := cryptoCreate(client)
+	if err != nil {
+		panic(err)
+	}
+	privKey2, err := cryptoCreate(client)
+	if err != nil {
+		panic(err)
+	}
+	privKey3, err := cryptoCreate(client)
+	if err != nil {
+		panic(err)
+	}
+	topicKey := hedera.KeyListWithThreshold(1)
+	topicKey = topicKey.
+		Add(privKey1.PublicKey()).
+		Add(privKey2.PublicKey()).
+		Add(privKey3.PublicKey())
+	txID, err := hedera.NewTopicCreateTransaction().
+		SetSubmitKey(topicKey).
+		Execute(client)
+	if err != nil {
+		panic(err)
+	}
+	topicReceipt, err := txID.GetReceipt(client)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("TopicID: %v\n", topicReceipt.TopicID)
+	fmt.Println("--------------------------")
+	custodialKey := hedera.KeyListWithThreshold(3)
+	custodialKey = custodialKey.Add(privKey1.PublicKey()).Add(privKey2.PublicKey()).Add(privKey3.PublicKey())
+	// Creating Bridge threshold account
+	bridgeAccount, err := hedera.NewAccountCreateTransaction().
+		SetKey(custodialKey).
+		Execute(client)
+	if err != nil {
+		panic(err)
+	}
+	bridgeAccountReceipt, err := bridgeAccount.GetReceipt(client)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Bridge Account: %v\n", bridgeAccountReceipt.AccountID)
+	fmt.Println("--------------------------")
+
+	// Creating Scheduled transaction payer theshhold account
+	scheduledTxPayerAccount, err := hedera.NewAccountCreateTransaction().
+		SetKey(custodialKey).
+		SetInitialBalance(balance).
+		Execute(client)
+	if err != nil {
+		panic(err)
+	}
+	scheduledTxPayerAccountReceipt, err := scheduledTxPayerAccount.GetReceipt(client)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Scheduled Tx Payer Account: %v\n", scheduledTxPayerAccountReceipt.AccountID)
+	fmt.Printf("Balance: %v\n HBars", balance)
+	fmt.Println("---Executed Successfully---")
+}
+func cryptoCreate(client *hedera.Client) (hedera.PrivateKey, error) {
+	privateKey, _ := hedera.GeneratePrivateKey()
+	fmt.Printf("Hedera Private Key: %v\n", privateKey.String())
+	publicKey := privateKey.PublicKey()
+	newAccount, err := hedera.NewAccountCreateTransaction().
+		SetKey(publicKey).
+		SetInitialBalance(balance).
+		Execute(client)
+	if err != nil {
+		return hedera.PrivateKey{}, err
+	}
+	receipt, err := newAccount.GetReceipt(client)
+	if err != nil {
+		return hedera.PrivateKey{}, err
+	}
+	fmt.Printf("AccountID: %v\n", receipt.AccountID)
+	fmt.Printf("Balance: %v\n HBars", balance)
+	fmt.Println("--------------------------")
+	return privateKey, nil
+}
+func initClient(privateKey, accountID, network string) *hedera.Client {
+	var client *hedera.Client
+
+	if network == "previewnet" {
+		client = hedera.ClientForPreviewnet()
+	} else if network == "testnet" {
+		client = hedera.ClientForTestnet()
+	} else if network == "mainnet" {
+		client = hedera.ClientForMainnet()
+	} else {
+		panic("Unknown Network Type!")
+	}
+	accID, err := hedera.AccountIDFromString(accountID)
+	if err != nil {
+		panic(err)
+	}
+	pK, err := hedera.PrivateKeyFromString(privateKey)
+	if err != nil {
+		panic(err)
+	}
+	client.SetOperator(accID, pK)
+	return client
+}
