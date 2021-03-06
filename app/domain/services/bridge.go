457da404deb15f21f56a675493142aa71e074070
Daniel Ivanov <daniel.k.ivanov95@gmail.com>
2021-03-06 18:49:50 +0200

    add bridge and memo files
    Signed-off-by: Daniel Ivanov <daniel.k.ivanov95@gmail.com>

---
+++ app/domain/services/bridge.go
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2021 LimeChain Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package services
+
+import (
+	hederaAPIModel "github.com/limechain/hedera-eth-bridge-validator/app/clients/hedera"
+	"github.com/limechain/hedera-eth-bridge-validator/app/encoding/memo"
+)
+
+// Bridge is the major service used for processing Bridge operations
+type Bridge interface {
+	// SanityCheck performs any validation required prior to handling the transaction (memo, state proof verification)
+	SanityCheck(tx hederaAPIModel.Transaction) (*memo.Memo, error)
+}
