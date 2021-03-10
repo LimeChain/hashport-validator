575388b660252f1dcb064b1ca30f2b76163e0841
failfmi <oscurocalma@gmail.com>
2021-03-10 16:09:50 +0200

    fix: update deprecated goethereum method (#128)
    * bump `goethereum` version
    * update deprecated `KeyTransactor` method
    Signed-off-by: failfmi <oscurocalma@gmail.com>

--- app/clients/ethereum/ethereum.go
+++ app/clients/ethereum/ethereum.go
@@ -26,12 +26,14 @@ import (
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/limechain/hedera-eth-bridge-validator/config"
 	log "github.com/sirupsen/logrus"
+	"math/big"
 	"time"
 )
 
 // Client Ethereum JSON RPC Client
 type Client struct {
-	config config.Ethereum
+	chainId *big.Int
+	config  config.Ethereum
 	*ethclient.Client
 }
 
@@ -42,10 +44,20 @@ func NewClient(config config.Ethereum) *Client {
 		log.Fatalf("Failed to initialize Client. Error [%s]", err)
 	}
 
+	chainId, err := client.ChainID(context.Background())
+	if err != nil {
+		log.Fatalf("Failed to get ChainID. Error [%s]", err)
+	}
+
 	return &Client{
+		chainId,
 		config,
 		client,
 	}
+}
+
+func (ec *Client) ChainID() *big.Int {
+	return ec.chainId
 }
 
 // GetClients returns the instance of a ethclient already established connection to a JSON RPC Ethereum Node
