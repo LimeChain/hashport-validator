8022e1c14bd10a1c3930aabb742d773d407328cf
Georgi Yazovaliyski <georgi.yazovaliiski@gmail.com>
2021-03-18 18:50:37 +0200

    Query Modification Modification
    Signed-off-by: Georgi Yazovaliyski <georgi.yazovaliiski@gmail.com>

--- app/persistence/message/message.go
+++ app/persistence/message/message.go
@@ -81,22 +81,20 @@ func (m Repository) GetMessagesFor(txId string) ([]TransactionMessage, error) {
 	return messages, nil
 }
 
-// TODO Move to message repo
 func (m *Repository) GetUnprocessedMessages() ([]*TransactionMessage, error) {
 	var messages []*TransactionMessage
 
-	// TODO
-	// Get all Message records which have TXID = ONE OF (Select TXID where Status = RECOVERED || INITIAL)
 	err := m.dbClient.Preload("transactions").Raw("SELECT " +
-		"transactions.transaction_id, " +
+		"transaction_messages.transaction_id, " +
 		"transaction_messages.eth_address, " +
 		"transaction_messages.amount, " +
 		"transaction_messages.fee, " +
 		"transaction_messages.signature, " +
+		"transaction_messages.erc20_contract_address, " +
 		"transaction_messages.gas_price_wei " +
-		"FROM transactions " +
-		"LEFT JOIN transaction_messages ON transactions.transaction_id = transaction_messages.transaction_id " +
-		"WHERE transactions.status = 'RECOVERED' OR transactions.status = 'INITIAL' ").
+		"FROM transaction_messages " +
+		"RIGHT JOIN transactions ON transaction_messages.transaction_id = transactions.transaction_id " +
+		"WHERE transactions.status = 'RECOVERED' OR transactions.status = 'INITIAL'").
 		Scan(&messages).Error
 	if err != nil {
 		return nil, err
