8e2976b8cd128d202640b890a688ece306e012f0
Ventsi Tsochev <ventsi@limechain.tech>
2021-05-17 17:35:09 +0300

    license added
    Signed-off-by: holymir <vtsochev@abv.bg>

--- test/test-config/test-config.go
+++ test/test-config/test-config.go
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021 LimeChain Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package test_config
 
 import "github.com/limechain/hedera-eth-bridge-validator/config"
@@ -20,7 +36,7 @@ var (
 					NodeUrl:               "wss://ropsten.infura.io/ws/v3/8b64d65996d24dc0aae2e0c6029e5a9b",
 					RouterContractAddress: "B5762f4159e7bFE24B5E7E9a2e829F535744d30e",
 					BlockConfirmations:    5,
-					PrivateKey:            "9f6da11eecc0fd7cb081d2aee88092ee3436397916c894ad6cd90a79009c0ded",
+					PrivateKey:            "9f6da11eecc0fd7cb081d2aee88092ee3436397916c894ad6cd80a79009c0ded",
 				},
 				MirrorNode: config.MirrorNode{
 					ClientAddress:   "",
@@ -32,7 +48,7 @@ var (
 					NetworkType: "testnet",
 					Operator: config.Operator{
 						AccountId:  "0.0.478300",
-						PrivateKey: "302e020100300506032b657004220420479934e1729d3a2a25f3cdec95862d247944635113b4f4a07ec44c5ff8ec0884",
+						PrivateKey: "302e020100300506032b657004220420479934e1729d3a2a25f3cdec95862d247944635113b4f4a07ec44c5ff8ec0885",
 					},
 					BridgeAccount: "0.0.476139",
 					PayerAccount:  "0.0.476139",
