c02c1bcf10d0f983ac5cd30073a0f5938a5bcf2c
Ventsi Tsochev <ventsi@limechain.tech>
2021-05-17 15:10:15 +0300

    helpers unit tests (#237)
    * helpers unit tests
    * assertion improvements
    * coverage added

---
+++ app/helper/big-numbers/helper_test.go
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2021 LimeChain Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package big_numbers
+
+import (
+	"github.com/stretchr/testify/assert"
+	"math/big"
+	"testing"
+)
+
+const (
+	validNumber            = "54321"
+	notValidNumber   = "0xsomerouteraddress"
+
+)
+
+func Test_StringToBigInt(t *testing.T) {
+	value, err := ToBigInt(validNumber)
+	assert.IsType(t, big.Int{}, *value)
+	assert.Nil(t, err)
+}
+
+func Test_ToBigIntError(t *testing.T) {
+	_, err := ToBigInt(notValidNumber)
+	assert.Error(t, err)
+}
---
+++ app/helper/hedera/token_test.go
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2021 LimeChain Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package hedera
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+const (
+	validTokenID = "0.0.1234"
+	invalidTokenID = "0.01234"
+)
+func Test_IsTokenID(t *testing.T) {
+	res := IsTokenID(validTokenID)
+	assert.True(t, res)
+}
+
+func Test_IsTokenIDError(t *testing.T) {
+	res := IsTokenID(invalidTokenID)
+	assert.False(t, res)
+}
