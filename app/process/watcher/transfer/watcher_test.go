eecc75226db526a8f8d429d818769ff61d3f4887
failfmi <oscurocalma@gmail.com>
2021-09-03 18:03:55 +0300

    test(transfer-watcher): failing not compiled tests

--- app/process/watcher/transfer/watcher_test.go
+++ app/process/watcher/transfer/watcher_test.go
@@ -20,6 +20,7 @@ import (
 	mirror_node "github.com/limechain/hedera-eth-bridge-validator/app/clients/hedera/mirror-node"
 	service2 "github.com/limechain/hedera-eth-bridge-validator/app/domain/service"
 	"github.com/limechain/hedera-eth-bridge-validator/config"
+	"github.com/limechain/hedera-eth-bridge-validator/config/parser"
 	"github.com/limechain/hedera-eth-bridge-validator/test/mocks"
 	"github.com/stretchr/testify/mock"
 	"testing"
@@ -35,41 +36,23 @@ var (
 			},
 		},
 	}
-	onlyNativeToWrapped = config.AssetMappings{
-		NativeToWrappedByNetwork: map[int64]config.Network{
-			0: {
-				NativeAssets: map[string]map[int64]string{
-					"0.0.111111": {
+	networks = map[int64]*parser.Network{
+		0: {
+			Tokens: map[string]parser.Token{
+				"0.0.111111": {
+					Networks: map[int64]string{
 						3: "0xevmaddress",
 					},
 				},
 			},
 		},
 	}
-	onlyWrappedToNative = config.AssetMappings{
-		NativeToWrappedByNetwork: map[int64]config.Network{
-			3: {
-				NativeAssets: map[string]map[int64]string{
-					"0xevmaddress": {
-						0: "0.0.11111",
-					},
-				},
-			},
-		},
-		WrappedToNativeByNetwork: map[int64]map[string]*config.NativeAsset{
-			0: {
-				"0.0.111111": &config.NativeAsset{
-					ChainId: 3,
-					Asset:   "0xevmaddress",
-				},
-			},
-		},
-	}
+	assets = config.LoadAssets(networks)
 )
 
 func Test_NewMemo_MissingWrappedCorrelation(t *testing.T) {
 	w := initializeWatcher()
-	mocks.MTransferService.On("SanityCheckTransfer", mock.Anything).Return(int64(3), "0xevmaddress", nil)
+	mocks.MTransferService.On("SanityCheckTransfer", mock.Anything).Return(int64(0), "0xevmaddress", nil)
 
 	w.processTransaction(tx, mocks.MQueue)
 	mocks.MTransferService.AssertCalled(t, "SanityCheckTransfer", tx)
@@ -81,8 +64,6 @@ func Test_NewMemo_CorrectCorrelation(t *testing.T) {
 	mocks.MTransferService.On("SanityCheckTransfer", mock.Anything).Return(int64(3), "0xevmaddress", nil)
 	mocks.MQueue.On("Push", mock.Anything).Return()
 
-	w.mappings = onlyNativeToWrapped
-
 	w.processTransaction(tx, mocks.MQueue)
 	mocks.MTransferService.AssertCalled(t, "SanityCheckTransfer", tx)
 	mocks.MQueue.AssertCalled(t, "Push", mock.Anything)
@@ -92,8 +73,6 @@ func Test_NewMemo_CorrectCorrelation_OnlyWrappedAssets(t *testing.T) {
 	w := initializeWatcher()
 	mocks.MTransferService.On("SanityCheckTransfer", mock.Anything).Return(int64(3), "0xevmaddress", nil)
 	mocks.MQueue.On("Push", mock.Anything).Return()
-
-	w.mappings = onlyWrappedToNative
 
 	w.processTransaction(tx, mocks.MQueue)
 	mocks.MTransferService.AssertCalled(t, "SanityCheckTransfer", tx)
@@ -111,5 +90,5 @@ func initializeWatcher() *Watcher {
 		mocks.MStatusRepository,
 		0,
 		map[int64]service2.Contracts{3: mocks.MBridgeContractService, 0: mocks.MBridgeContractService},
-		config.AssetMappings{})
+		assets)
 }
