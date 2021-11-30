afbea6cf78c1e0519117d560873d510eaccdc525
failfmi <oscurocalma@gmail.com>
2021-11-30 12:30:47 +0200

    fix: no need to double get native asset
    Signed-off-by: failfmi <oscurocalma@gmail.com>

--- app/process/watcher/transfer/watcher.go
+++ app/process/watcher/transfer/watcher.go
@@ -168,10 +168,7 @@ func (ctw Watcher) processTransaction(tx model.Transaction, q qi.Queue) {
 		return
 	}
 
-	nativeAsset := &config.NativeAsset{
-		ChainId: 0,
-		Asset:   asset,
-	}
+	nativeAsset := ctw.mappings.FungibleNativeAsset(0, asset)
 	targetChainAsset := ctw.mappings.NativeToWrapped(asset, 0, targetChainId)
 	if targetChainAsset == "" {
 		nativeAsset = ctw.mappings.WrappedToNative(asset, 0)
@@ -202,7 +199,6 @@ func (ctw Watcher) processTransaction(tx model.Transaction, q qi.Queue) {
 			err)
 		return
 	}
-	nativeAsset = ctw.mappings.FungibleNativeAsset(nativeAsset.ChainId, nativeAsset.Asset)
 	if properAmount.Cmp(nativeAsset.MinAmount) < 0 {
 		ctw.logger.Errorf(
 			"[%s] - Transfer Amount [%s] is less than minimum Amount [%s]",
