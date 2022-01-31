8a2ec7cd943bd6aa91aa61abde2cd6eddb79b2b7
nnedkov-limechain <nikolay.nedkov@limechain.tech>
2022-01-31 11:51:55 +0200

    Adding checks for prometheus service if it is nil in evm watcher.
    Signed-off-by: nnedkov-limechain <nikolay.nedkov@limechain.tech>

--- app/process/watcher/evm/watcher.go
+++ app/process/watcher/evm/watcher.go
@@ -589,6 +589,10 @@ func (ew *Watcher) setUserGetHisTokensMetric(transactionId string, sourceChainId
 }
 
 func (ew *Watcher) initializeUserGetHisTokens(transactionId string, sourceChainId int64, targetChainId int64, asset string) (prometheus.Gauge, error) {
+	if ew.prometheusService == nil {
+		return nil, nil
+	}
+
 	// User Get His Tokens
 	gauge, err := ew.prometheusService.CreateAndRegisterGaugeMetricForSuccessRate(
 		transactionId,
@@ -608,7 +612,7 @@ func (ew *Watcher) initializeUserGetHisTokens(transactionId string, sourceChainI
 }
 
 func (ew *Watcher) initializeMajorityReachedMetric(transactionId string, sourceChainId int64, targetChainId int64, asset string) (prometheus.Gauge, error) {
-	if sourceChainId == constants.HederaNetworkId || targetChainId == constants.HederaNetworkId {
+	if ew.prometheusService == nil || sourceChainId == constants.HederaNetworkId || targetChainId == constants.HederaNetworkId {
 		return nil, nil
 	}
 	gauge, err := ew.prometheusService.CreateAndRegisterGaugeMetricForSuccessRate(
