803c4248db44fab76012f31dec4caa2c754073b1
nnedkov-limechain <nikolay.nedkov@limechain.tech>
2022-02-08 10:16:07 +0200

    Adding missing check in handleUnlockLog
    Signed-off-by: nnedkov-limechain <nikolay.nedkov@limechain.tech>

--- e2e/e2e_test.go
+++ e2e/e2e_test.go
@@ -538,7 +538,7 @@ func Test_EVM_Native_to_EVM_Token(t *testing.T) {
 	chainId := int64(80001) // represents Polygon Mumbai Testnet (e2e config must have configuration for that particular network)
 	evm := setupEnv.Clients.EVM[chainId]
 	now = time.Now()
-	targetChainID := int64(3) // represents Avalanche Fuji Testnet (e2e config must have configuration for that particular network)
+	targetChainID := int64(43113) // represents Avalanche Fuji Testnet (e2e config must have configuration for that particular network)
 	wrappedAsset, err := setup.NativeToWrappedAsset(setupEnv.AssetMappings, chainId, targetChainID, setupEnv.NativeEvmToken)
 	if err != nil {
 		t.Fatal(err)
