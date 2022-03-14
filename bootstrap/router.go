823243af2f8aaed2a3b1b706d1b42a59bdd350a5
Nikolay Nedkov <nikolay.nedkov@limechain.tech>
2022-03-14 12:48:51 +0200

    Moving hardcoded '/metrics' endpoint to constants/prometheus.go
    Signed-off-by: Nikolay Nedkov <nikolay.nedkov@limechain.tech>

--- bootstrap/router.go
+++ bootstrap/router.go
@@ -8,6 +8,7 @@ import (
 	min_amounts "github.com/limechain/hedera-eth-bridge-validator/app/router/min-amounts"
 	"github.com/limechain/hedera-eth-bridge-validator/app/router/transfer"
 	"github.com/limechain/hedera-eth-bridge-validator/config/parser"
+	"github.com/limechain/hedera-eth-bridge-validator/constants"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
@@ -16,7 +17,7 @@ func InitializeAPIRouter(services *Services, bridgeConfig parser.Bridge) *apirou
 	apiRouter.AddV1Router(healthcheck.Route, healthcheck.NewRouter())
 	apiRouter.AddV1Router(transfer.Route, transfer.NewRouter(services.transfers))
 	apiRouter.AddV1Router(burn_event.Route, burn_event.NewRouter(services.BurnEvents))
-	apiRouter.AddV1Router("/metrics", promhttp.Handler())
+	apiRouter.AddV1Router(constants.PrometheusMetricsEndpoint, promhttp.Handler())
 	apiRouter.AddV1Router(config_bridge.Route, config_bridge.NewRouter(bridgeConfig))
 	apiRouter.AddV1Router(min_amounts.Route, min_amounts.NewRouter(services.Pricing))
 
