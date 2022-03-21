a0b6d47ced5ad3a612848eab437f494f22f7bd27
Nikolay Nedkov <nikolay.nedkov@limechain.tech>
2022-03-21 18:27:15 +0200

    Trying to fix go-test.yaml
    Signed-off-by: Nikolay Nedkov <nikolay.nedkov@limechain.tech>

--- .github/workflows/scripts/codecov.sh
+++
@@ -1,7 +0,0 @@
-for d in $(go list ./... | grep -v e2e); do
-    go test -race -coverprofile=profile.out -covermode=atomic $d
-    if [ -f profile.out ]; then
-        cat profile.out >> coverage.txt
-        rm profile.out
-    fi
-done
