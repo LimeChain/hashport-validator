711605eac8ea07f1ac1455359f4bf5992b39f141
Nikolay Nedkov <nikolay.nedkov@limechain.tech>
2022-04-04 17:19:48 +0300

    Feature/unit tests for app router (#540)
    * Adding tests for 'app/routre/transfer'.
    
    Signed-off-by: Nikolay Nedkov <nikolay.nedkov@limechain.tech>
    
    * Adding tests for 'app/router/burn-event'.
    
    Signed-off-by: Nikolay Nedkov <nikolay.nedkov@limechain.tech>
    
    * Adding unit tests for 'app/router/response'.
    
    Signed-off-by: Nikolay Nedkov <nikolay.nedkov@limechain.tech>
    
    * Adding tests for 'app/router'.
    
    Signed-off-by: Nikolay Nedkov <nikolay.nedkov@limechain.tech>

---
+++ app/router/router_test.go
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2022 LimeChain Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package router
+
+import (
+	"github.com/limechain/hedera-eth-bridge-validator/test/mocks"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+var (
+	path = "/endpoint"
+)
+
+func Test_NewRouter(t *testing.T) {
+	actualRouter := NewAPIRouter()
+	actualMiddlewares := actualRouter.Router.Middlewares()
+
+	assert.Len(t, actualMiddlewares, len(middlewares))
+}
+
+func Test_AddV1Router(t *testing.T) {
+	router := NewAPIRouter()
+	router.AddV1Router(path, mocks.MHttpHandler)
+
+	routes := router.Router.Routes()
+
+	expectedPath := apiV1 + path + "/*"
+	assert.Len(t, routes, 1)
+	assert.Equal(t, expectedPath, routes[0].Pattern)
+}
