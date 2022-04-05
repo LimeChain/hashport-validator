2069fe3045e61dfbe2aa866f8f72f3b88b88bd5b
Boris Lachev <boris.lachev@icloud.com>
2022-04-05 12:23:50 +0300

    Add test for NewNft (#541)
    * Add test for NewNft
    
    Signed-off-by: Boris Lachev <boris.lachev@icloud.com>
    
    * Adding tests 'clients/hedera/mirror-node/model/error.go'. (#542)
    
    Signed-off-by: Nikolay Nedkov <nikolay.nedkov@limechain.tech>
    
    Co-authored-by: Nikolay Nedkov <nikolay.nedkov@limechain.tech>

--- app/model/transfer/transfer_test.go
+++ app/model/transfer/transfer_test.go
@@ -15,6 +15,8 @@ const (
 	sourceAsset   = "0.0.123"
 	nativeAsset   = "0.0.123"
 	targetAsset   = "0xwrapped00123"
+	serialNum     = 0
+	metadata      = "42"
 )
 
 func Test_New(t *testing.T) {
@@ -38,4 +40,34 @@ func Test_New(t *testing.T) {
 		sourceAsset,
 		amount)
 	assert.Equal(t, expectedTransfer, actualTransfer)
+}
+
+func Test_NewNft(t *testing.T) {
+	expected := &Transfer{
+		TransactionId: txId,
+		SourceChainId: sourceChainId,
+		TargetChainId: targetChainId,
+		NativeChainId: nativeChainId,
+		SourceAsset:   sourceAsset,
+		TargetAsset:   targetAsset,
+		NativeAsset:   nativeAsset,
+		Receiver:      receiver,
+		SerialNum:     serialNum,
+		Metadata:      metadata,
+		IsNft:         true,
+		Amount:        "",
+		Timestamp:     "",
+	}
+
+	actual := NewNft(txId,
+		sourceChainId,
+		targetChainId,
+		nativeChainId,
+		receiver,
+		sourceAsset,
+		targetAsset,
+		nativeAsset,
+		serialNum,
+		metadata)
+	assert.Equal(t, expected, actual)
 }
