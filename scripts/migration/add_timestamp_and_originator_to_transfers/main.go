986691b2862018f25cd27cd4cc0250217f2d8e13
Boris Lachev <boris.lachev@icloud.com>
2022-05-11 02:03:31 +0300

    Fix migration script
    Signed-off-by: Boris Lachev <boris.lachev@icloud.com>

--- scripts/migration/add_timestamp_and_originator_to_transfers/main.go
+++ scripts/migration/add_timestamp_and_originator_to_transfers/main.go
@@ -21,50 +21,54 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	hederahelper "github.com/limechain/hedera-eth-bridge-validator/app/helper/hedera"
+
+	"github.com/limechain/hedera-eth-bridge-validator/app/domain/client"
+	"github.com/limechain/hedera-eth-bridge-validator/app/persistence"
+
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/hashgraph/hedera-sdk-go/v2"
 	"github.com/limechain/hedera-eth-bridge-validator/app/clients/evm"
-	"github.com/limechain/hedera-eth-bridge-validator/app/domain/client"
-	"github.com/limechain/hedera-eth-bridge-validator/app/domain/repository"
-	"github.com/limechain/hedera-eth-bridge-validator/app/persistence"
-	"github.com/limechain/hedera-eth-bridge-validator/app/persistence/entity"
-	"github.com/limechain/hedera-eth-bridge-validator/app/persistence/transfer"
 	validatorCfg "github.com/limechain/hedera-eth-bridge-validator/config"
 	log "github.com/sirupsen/logrus"
-	"strconv"
-	"strings"
-	"time"
 )
+
+const evmCallTimeout = 5 * time.Second
 
 func main() {
 	cfg := new(config)
-	err := validatorCfg.GetConfig(cfg, "config.yml")
+	err := validatorCfg.GetConfig(cfg, "scripts/migration/add_timestamp_and_originator_to_transfers/config.yml")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// Setup hedera, evm clients and nodes to be migrated
-	hederaClient := setupHederaClient(cfg.Env)
 	nodes := make([]*node, len(cfg.DBs))
 	for i, db := range cfg.DBs {
 		pgDb := persistence.NewPgConnector(db).Connect()
-
-		nodes[i].db, err = pgDb.DB()
-		nodes[i].repo = transfer.NewRepository(pgDb)
-
+		db, err := pgDb.DB()
 		if err != nil {
 			log.Fatal(err)
+		}
+
+		nodes[i] = &node{
+			db: db,
 		}
 	}
 	evmClients := make(map[uint64]client.Core)
 	for k, v := range cfg.Evm {
 		evmClients[k] = evm.NewClient(validatorCfg.Evm{
-			NodeUrl: v,
+			NodeUrl:            v,
+			BlockConfirmations: 5,
 		}, k)
 	}
 
-	migrator := newMigrator(nodes, hederaClient, evmClients)
+	migrator := newMigrator(nodes, evmClients)
 
 	log.Infof("finna update %d nodes using %d evm clients", len(migrator.nodes), len(migrator.evmClients))
 	err = migrator.migrate()
@@ -73,30 +77,42 @@ func main() {
 	}
 }
 
+type tempTransfer struct {
+	TransactionID string
+	SourceChainID uint64
+	Timestamp     sql.NullTime
+	Originator    sql.NullString
+}
+
+type hederaCfg struct {
+	Env        hedera.NetworkName      `yaml:"env"`
+	AccountId  string                  `yaml:"account_id"`
+	PrivateKey string                  `yaml:"private_key"`
+	MirrorNode validatorCfg.MirrorNode `yaml:"mirror_node"`
+}
+
 type config struct {
-	Env hedera.NetworkName      `yaml:"env"`
-	DBs []validatorCfg.Database `yaml:"dbs"`
-	Evm map[uint64]string       `yaml:"evm"`
+	Hedera hederaCfg               `yaml:"hedera"`
+	DBs    []validatorCfg.Database `yaml:"dbs"`
+	Evm    map[uint64]string       `yaml:"evm"`
 }
 
 type node struct {
-	db   *sql.DB
-	repo repository.Transfer
+	db *sql.DB
 }
 
 type migrator struct {
-	nodes        []*node
-	hederaClient *hedera.Client
-	evmClients   map[uint64]client.Core
+	nodes      []*node
+	evmClients map[uint64]client.Core
 }
 
-func newMigrator(nodes []*node, hederaClient *hedera.Client, evmClients map[uint64]client.Core) *migrator {
-	return &migrator{nodes: nodes, hederaClient: hederaClient, evmClients: evmClients}
+func newMigrator(nodes []*node, evmClients map[uint64]client.Core) *migrator {
+	return &migrator{nodes: nodes, evmClients: evmClients}
 }
 
 func (m *migrator) migrate() error {
-	for i, node := range m.nodes {
-		err := m.migrateNode(node, i)
+	for i, _ := range m.nodes {
+		err := m.migrateNode(i)
 		if err != nil {
 			return err
 		}
@@ -104,28 +120,34 @@ func (m *migrator) migrate() error {
 	return nil
 }
 
-func (m *migrator) migrateNode(node *node, i int) error {
-	c, err := node.db.Query(`select count (*) from transfers`)
+func (m *migrator) migrateNode(i int) error {
+	migratedNode := m.nodes[i]
+	c, err := migratedNode.db.Query(`select count (*) from transfers`)
 	if err != nil {
 		return err
 	}
 
-	var count int64
-	if err := c.Scan(&count); err != nil {
-		return err
+	var count int
+	if c.Next() {
+		if err := c.Scan(&count); err != nil {
+			return err
+		}
+	} else {
+		return errors.New("no transfers count")
 	}
+	c.Close()
 
-	tx, err := node.db.Begin()
+	tx, err := migratedNode.db.Begin()
 	if err != nil {
 		return err
 	}
 	defer tx.Rollback()
 
 	page := 1
-	const perPage = 100
-	totalUpdated := int64(0)
+	const perPage = 5
+	totalUpdated := 0
 	for {
-		if totalUpdated == count {
+		if totalUpdated >= count {
 			if err := tx.Commit(); err != nil {
 				return err
 			}
@@ -133,51 +155,55 @@ func (m *migrator) migrateNode(node *node, i int) error {
 			break
 		}
 
-		transfers, err := node.repo.PagedOffset(page, perPage)
+		transfers, err := migratedNode.pagedTransfers(page, perPage)
 		if err != nil {
 			return err
 		}
 
 		for _, t := range transfers {
+			if t.Originator.Valid && t.Timestamp.Valid {
+				continue
+			}
+
 			err := m.fillTimestampAndOriginator(t)
 			if err != nil {
 				return err
 			}
 		}
 
-		q := m.prepareQuery(transfers)
-		res, err := tx.Exec(q)
-		if err != nil {
-			return err
-		}
-		r, err := res.RowsAffected()
+		qs := m.prepareQuery(transfers)
 		if err != nil {
 			return err
 		}
 
-		totalUpdated += r
-		log.Infof("updated %d rows", r)
+		for _, q := range qs {
+			if _, err := tx.Exec(q); err != nil {
+				return err
+			}
+		}
+		totalUpdated += len(qs)
+		log.Infof("updated %d/%d transfers", totalUpdated, count)
+
+		page++
 	}
 
 	return nil
 }
 
-func (m *migrator) hederaFields(transfer *entity.Transfer) error {
-	txId, err := hedera.TransactionIdFromString(transfer.TransactionID)
-	tx, err := hedera.NewTransactionRecordQuery().
-		SetTransactionID(txId).
-		Execute(m.hederaClient)
+func (m *migrator) hederaFields(tr *tempTransfer) error {
+	o := hederahelper.OriginatorFromTxId(tr.TransactionID)
+	t, err := hederahelper.TimestampFromTxId(tr.TransactionID)
 	if err != nil {
 		return err
 	}
 
-	transfer.Timestamp = tx.ConsensusTimestamp
-	transfer.Originator = tx.Receipt.AccountID.String()
+	tr.Timestamp = sql.NullTime{Time: t.UTC(), Valid: true}
+	tr.Originator = sql.NullString{String: o, Valid: true}
 
 	return nil
 }
 
-func (m *migrator) evmFields(transfer *entity.Transfer) error {
+func (m *migrator) evmFields(transfer *tempTransfer) error {
 	parts := strings.SplitN(transfer.TransactionID, "-", 2)
 	if len(parts) != 2 {
 		return errors.New("transfer is not an evm transfer")
@@ -189,7 +215,7 @@ func (m *migrator) evmFields(transfer *entity.Transfer) error {
 		return err
 	}
 
-	t1, cancel1 := context.WithTimeout(context.Background(), time.Second*5)
+	t1, cancel1 := context.WithTimeout(context.Background(), evmCallTimeout)
 	defer cancel1()
 
 	rx, err := c.TransactionReceipt(t1, common.HexToHash(txHash))
@@ -197,7 +223,7 @@ func (m *migrator) evmFields(transfer *entity.Transfer) error {
 		return err
 	}
 
-	t2, cancel2 := context.WithTimeout(context.Background(), time.Second*5)
+	t2, cancel2 := context.WithTimeout(context.Background(), evmCallTimeout)
 	defer cancel2()
 
 	block, err := c.BlockByNumber(t2, rx.BlockNumber)
@@ -205,10 +231,10 @@ func (m *migrator) evmFields(transfer *entity.Transfer) error {
 		return err
 	}
 
-	uT := time.Unix(0, int64(block.Time()))
-	transfer.Timestamp = uT
+	uT := time.Unix(int64(block.Time()), 0)
+	transfer.Timestamp = sql.NullTime{Time: uT.UTC()}
 
-	t3, cancel3 := context.WithTimeout(context.Background(), time.Second*5)
+	t3, cancel3 := context.WithTimeout(context.Background(), evmCallTimeout)
 	defer cancel3()
 
 	tx, pending, err := c.TransactionByHash(t3, common.HexToHash(txHash))
@@ -219,18 +245,18 @@ func (m *migrator) evmFields(transfer *entity.Transfer) error {
 		return errors.New("pending transaction")
 	}
 
-	msg, err := tx.AsMessage(types.NewEIP155Signer(tx.ChainId()), nil)
+	msg, err := tx.AsMessage(types.LatestSignerForChainID(tx.ChainId()), nil)
 	if err != nil {
 		return err
 	}
 
-	transfer.Originator = msg.From().String()
+	transfer.Originator = sql.NullString{String: msg.From().String()}
 
 	return nil
 }
 
-func (m *migrator) fillTimestampAndOriginator(transfer *entity.Transfer) error {
-	if strings.Contains("0x", transfer.TransactionID) {
+func (m *migrator) fillTimestampAndOriginator(transfer *tempTransfer) error {
+	if strings.Contains(transfer.TransactionID, "0x") {
 		return m.evmFields(transfer)
 	}
 	return m.hederaFields(transfer)
@@ -244,28 +270,36 @@ func (m *migrator) evmClientFor(networkId uint64) (client.Core, error) {
 	return c, nil
 }
 
-func (m *migrator) prepareQuery(transfers []*entity.Transfer) string {
-	var q string
+func (m *migrator) prepareQuery(transfers []*tempTransfer) []string {
+	qs := make([]string, 0, len(transfers))
 	for _, t := range transfers {
-		q += fmt.Sprintf(
-			`update transfers set timestamp = '%s', originator = '%s' where transaction_id = '%s';`,
-			t.Timestamp.Format(time.RFC3339), t.Originator, t.TransactionID)
+		q := fmt.Sprintf(
+			"update transfers set \"timestamp\" = '%s', \"originator\" = '%s' where \"transaction_id\" = '%s';\n",
+			t.Timestamp.Time.UTC().Format(time.RFC3339), t.Originator.String, t.TransactionID)
+		qs = append(qs, q)
 	}
 
-	return q
+	return qs
 }
 
-func (m *migrator) name() {
-
-}
-
-func setupHederaClient(env hedera.NetworkName) *hedera.Client {
-	var res *hedera.Client
-	switch env {
-	case hedera.NetworkNameTestnet:
-		res = hedera.ClientForTestnet()
-	case hedera.NetworkNameMainnet:
-		res = hedera.ClientForMainnet()
+func (n *node) pagedTransfers(page, perPage int) ([]*tempTransfer, error) {
+	offset := (page - 1) * perPage
+	cur, err := n.db.Query(`
+		select "transaction_id", "source_chain_id", "timestamp", "originator" from "transfers"
+		offset $1 limit $2`,
+		offset, perPage)
+	if err != nil {
+		return nil, err
 	}
-	return res
+
+	res := make([]*tempTransfer, 0, perPage)
+	for cur.Next() {
+		var t tempTransfer
+		if err := cur.Scan(&t.TransactionID, &t.SourceChainID, &t.Timestamp, &t.Originator); err != nil {
+			return nil, err
+		}
+		res = append(res, &t)
+	}
+
+	return res, nil
 }
