1428a973f66c450b82b88c6e69f23b1d9062992e
Boris Lachev <boris.lachev@icloud.com>
2022-07-05 15:22:46 +0300

    Add default and docs
    Signed-off-by: Boris Lachev <boris.lachev@icloud.com>

--- config/parser/node.go
+++ config/parser/node.go
@@ -65,7 +65,7 @@ type Hedera struct {
 	Network        string            `yaml:"network"`
 	Rpc            map[string]string `yaml:"rpc"`
 	StartTimestamp int64             `yaml:"start_timestamp"`
-	MaxRetry       int               `yaml:"max_retry"`
+	MaxRetry       int               `yaml:"max_retry" default:"5"`
 }
 
 type Operator struct {
