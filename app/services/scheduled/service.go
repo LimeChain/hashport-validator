0b93e4af4fa2f51f02e2189cf9bcd05d0ebca6a1
Nikolay Nedkov <nikolay.nedkov@limechain.tech>
2022-09-09 11:43:36 +0300

    Adding logs for Node account IDs for which a scheduled transaction is… (#732)
    * Adding logs for Node account IDs for which a scheduled transaction is being signed. And on which node was executed when there is an error.
    
    Signed-off-by: Nikolay Nedkov <nikolay.nedkov@limechain.tech>
    
    * Fixing statickcheck errors.
    
    Signed-off-by: Nikolay Nedkov <nikolay.nedkov@limechain.tech>
    
    * Adding logs on failed scheduled burn and mint transactions.
    
    Signed-off-by: Nikolay Nedkov <nikolay.nedkov@limechain.tech>
    
    Signed-off-by: Nikolay Nedkov <nikolay.nedkov@limechain.tech>

--- app/services/scheduled/service.go
+++ app/services/scheduled/service.go
@@ -44,9 +44,11 @@ func (s *Service) ExecuteScheduledTransferTransaction(
 	onExecutionSuccess func(transactionID, scheduleID string), onExecutionFail, onSuccess, onFail func(transactionID string)) {
 	transactionResponse, err := s.executeScheduledTransfersTransaction(id, nativeAsset, transfers)
 	if err != nil {
-		s.logger.Errorf("[%s] - Failed to submit scheduled transfer transaction. Error [%s].", id, err)
 		if transactionResponse != nil {
 			onExecutionFail(hederahelper.ToMirrorNodeTransactionID(transactionResponse.TransactionID.String()))
+			s.logger.Errorf("[%s] - Failed to submit scheduled transfer transaction at Node Account [%s]. Error [%s].", id, transactionResponse.NodeID, err)
+		} else {
+			s.logger.Errorf("[%s] - Failed to submit scheduled transfer transaction. Error [%s].", id, err)
 		}
 		return
 	}
@@ -63,9 +65,11 @@ func (s *Service) ExecuteScheduledNftTransferTransaction(
 	onExecutionSuccess func(transactionID, scheduleID string), onExecutionFail, onSuccess, onFail func(transactionID string)) {
 	transactionResponse, err := s.hederaNodeClient.SubmitScheduledNftTransferTransaction(nftID, s.payerAccount, sender, receiving, id)
 	if err != nil {
-		s.logger.Errorf("[%s] - Failed to submit scheduled transfer transaction. Error [%s].", id, err)
 		if transactionResponse != nil {
 			onExecutionFail(hederahelper.ToMirrorNodeTransactionID(transactionResponse.TransactionID.String()))
+			s.logger.Errorf("[%s] - Failed to submit scheduled transfer transaction at Node Account [%s]. Error [%s].", id, transactionResponse.NodeID.String(), err)
+		} else {
+			s.logger.Errorf("[%s] - Failed to submit scheduled transfer transaction. Error [%s].", id, err)
 		}
 		return
 	}
@@ -99,9 +103,11 @@ func (s *Service) executeScheduledTransfersTransaction(id, nativeAsset string, t
 func (s *Service) ExecuteScheduledMintTransaction(id, asset string, amount int64, status *chan string, onExecutionSuccess func(transactionID, scheduleID string), onExecutionFail, onSuccess, onFail func(transactionID string)) {
 	transactionResponse, err := s.executeScheduledTokenMintTransaction(id, asset, amount)
 	if err != nil {
-		s.logger.Errorf("[%s] - Failed to submit scheduled mint transaction. Error [%s].", id, err)
 		if transactionResponse != nil {
 			onExecutionFail(hederahelper.ToMirrorNodeTransactionID(transactionResponse.TransactionID.String()))
+			s.logger.Errorf("[%s] - Failed to submit scheduled mint transaction at Node Account [%s]. Error [%s].", id, transactionResponse.NodeID.String(), err)
+		} else {
+			s.logger.Errorf("[%s] - Failed to submit scheduled mint transaction. Error [%s].", id, err)
 		}
 		*status <- sync.FAIL
 		return
@@ -118,9 +124,11 @@ func (s *Service) ExecuteScheduledMintTransaction(id, asset string, amount int64
 func (s *Service) ExecuteScheduledBurnTransaction(id, asset string, amount int64, status *chan string, onExecutionSuccess func(transactionID, scheduleID string), onExecutionFail, onSuccess, onFail func(transactionID string)) {
 	transactionResponse, err := s.executeScheduledTokenBurnTransaction(id, asset, amount)
 	if err != nil {
-		s.logger.Errorf("[%s] - Failed to submit scheduled burn transaction. Error [%s].", id, err)
 		if transactionResponse != nil {
 			onExecutionFail(hederahelper.ToMirrorNodeTransactionID(transactionResponse.TransactionID.String()))
+			s.logger.Errorf("[%s] - Failed to submit scheduled burn transaction at Node Account [%s]. Error [%s].", id, transactionResponse.NodeID.String(), err)
+		} else {
+			s.logger.Errorf("[%s] - Failed to submit scheduled burn transaction. Error [%s].", id, err)
 		}
 		*status <- sync.FAIL
 		return
