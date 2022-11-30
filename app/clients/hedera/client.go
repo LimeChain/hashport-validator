dd0155b1e62caf770f3246e74cc89cd26cf2e64f
Antonio Mindov <antonio.mindov@limechain.tech>
2022-11-30 14:11:42 +0200

    Added fix for INVALID_NODE_ACCOUNT (#780) (#783)
    Signed-off-by: Antonio Mindov <antonio.mindov@limechain.tech>
    
    Signed-off-by: Antonio Mindov <antonio.mindov@limechain.tech>

--- app/clients/hedera/client.go
+++ app/clients/hedera/client.go
@@ -124,14 +124,36 @@ func (hc Node) SubmitScheduledTokenBurnTransaction(tokenID hedera.TokenID, amoun
 // SubmitTopicConsensusMessage submits the provided message bytes to the
 // specified HCS `topicId`
 func (hc Node) SubmitTopicConsensusMessage(topicId hedera.TopicID, message []byte) (*hedera.TransactionID, error) {
-	tx, err := hedera.NewTopicMessageSubmitTransaction().
-		SetTopicID(topicId).
-		SetMessage(message).
-		SetMaxRetry(hc.maxRetry).
-		FreezeWith(hc.client)
+	attempt := 0
+	for attempt <= hc.maxRetry {
+		attempt++
+		tx, err := hedera.NewTopicMessageSubmitTransaction().
+			SetTopicID(topicId).
+			SetMessage(message).
+			SetMaxRetry(hc.maxRetry).
+			FreezeWith(hc.client)
 
-	if err != nil {
-		return nil, err
+		if err != nil {
+			return nil, err
+		}
+
+		hc.logger.Debugf("Submit Topic Consensus Message, with transaction ID [%s] and Node Account IDs: %v",
+			tx.GetTransactionID(),
+			tx.GetNodeAccountIDs(),
+		)
+
+		response, err := tx.Execute(hc.GetClient())
+		if shouldRetryTransaction(err) {
+			continue
+		}
+
+		if err != nil {
+			return nil, err
+		}
+
+		_, err = hc.checkTransactionReceipt(response)
+
+		return &response.TransactionID, err
 	}
 
 	hc.logger.Debugf("Submit Topic Consensus Message, with transaction ID [%s] and Node Account IDs: %v",
@@ -153,13 +175,29 @@ func (hc Node) SubmitTopicConsensusMessage(topicId hedera.TopicID, message []byt
 
 // SubmitScheduleSign submits a ScheduleSign transaction for a given ScheduleID
 func (hc Node) SubmitScheduleSign(scheduleID hedera.ScheduleID) (*hedera.TransactionResponse, error) {
-	tx, err := hedera.NewScheduleSignTransaction().
-		SetScheduleID(scheduleID).
-		SetMaxRetry(hc.maxRetry).
-		FreezeWith(hc.GetClient())
+	attempt := 0
+	for attempt <= hc.maxRetry {
+		attempt++
+		tx, err := hedera.NewScheduleSignTransaction().
+			SetScheduleID(scheduleID).
+			SetMaxRetry(hc.maxRetry).
+			FreezeWith(hc.GetClient())
 
-	if err != nil {
-		return nil, err
+		if err != nil {
+			return nil, err
+		}
+
+		hc.logger.Debugf("Submit Schedule Sign, with transaction ID [%s], schedule ID: [%s] and Node Account IDs: %v",
+			tx.GetTransactionID(),
+			tx.GetScheduleID(),
+			tx.GetNodeAccountIDs(),
+		)
+		response, err := tx.Execute(hc.GetClient())
+		if shouldRetryTransaction(err) {
+			continue
+		}
+
+		return &response, err
 	}
 
 	hc.logger.Debugf("Submit Schedule Sign, with transaction ID [%s], schedule ID: [%s] and Node Account IDs: %v",
@@ -269,14 +307,17 @@ func (hc Node) submitScheduledTransferTransaction(payerAccountID hedera.AccountI
 }
 
 func (hc Node) submitScheduledTransaction(signedTransaction hedera.ITransaction, payerAccountID hedera.AccountID, memo string) (*hedera.TransactionResponse, error) {
-	scheduledTx, err := hedera.NewScheduleCreateTransaction().
-		SetScheduledTransaction(signedTransaction)
-	if err != nil {
-		return nil, err
-	}
-	scheduledTx = scheduledTx.
-		SetPayerAccountID(payerAccountID).
-		SetScheduleMemo(memo)
+	attempt := 0
+	for attempt <= hc.maxRetry {
+		attempt++
+		scheduledTx, err := hedera.NewScheduleCreateTransaction().
+			SetScheduledTransaction(signedTransaction)
+		if err != nil {
+			return nil, err
+		}
+		scheduledTx = scheduledTx.
+			SetPayerAccountID(payerAccountID).
+			SetScheduleMemo(memo)
 
 	response, err := scheduledTx.Execute(hc.GetClient())
 
@@ -294,4 +335,17 @@ func (hc Node) checkTransactionReceipt(txResponse hedera.TransactionResponse) (*
 	}
 
 	return &receipt, err
+}
+
+func shouldRetryTransaction(err error) bool {
+	if err == nil {
+		return false
+	}
+
+	preCheckError, ok := err.(*hedera.ErrHederaPreCheckStatus)
+	if ok && preCheckError.Status == hedera.StatusInvalidNodeAccount {
+		return true
+	}
+
+	return false
 }
