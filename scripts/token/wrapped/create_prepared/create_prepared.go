7e9aee91c66a7be012f584010f567240758a60ec
Stanislav Viyachev <stanislav.viyachev@limechain.tech>
2023-03-21 17:14:09 +0200

    revision
    Signed-off-by: Stanislav Viyachev <stanislav.viyachev@limechain.tech>

--- scripts/token/wrapped/create_prepared/create_prepared.go
+++ scripts/token/wrapped/create_prepared/create_prepared.go
@@ -21,7 +21,7 @@ go run ./scripts/token/wrapped/create_prepared/create_prepared.go \
     --tokenName "Token Name" \
     --tokenSymbol "symbol[hts]" \
     --threshold 5 \
-    --memberKeys "25075f86a93019a22420c845b14add02c7f80acce5ee63aa2476cc928247e53a,5eca016e197434e11afd8008d39f83d2d6b7342dbb3745526b53dd4071f6eeeb,1b44857eb54139c20c2b3d548d8f45308d546a7a974f666a90435d48969834f3,ec4f80087c309eaf50d814b504d2be08577149456470a6484f34984ccbbcf4af,db439b30e19e66177dae79c4018e488ddeff2b62747ad2e4f7a8184cfd264a79,1e7129725857fbdae26ba032092c232fd3d720c60b0b292f25bea1166ca410b7,99ccf8a1f660d62f7268abe2b14357a4b11ab445ba04ccc7f3e842707b846a3f,bf2468d0475d4a9bf002801b20504e5db7c3a9f0593096f311815767401c51d4,5b6dd26306ecdc89013f067613afd535384a7c3b6abd96f224444e391c7d5bba" \
+    --memberKeys "" \
     --initialSupply 0 \
     --decimals 8 \
     --nodeAccountID 0.0.3
